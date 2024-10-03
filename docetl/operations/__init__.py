20a9c08ea40b86ca1e937b44e0f60d67cc347ebc
Shreya Shankar <ss.shankar505@gmail.com>
2024-10-02 23:43:23 -0700

    refactor: make sure tests pass

--- docetl/operations/__init__.py
+++ docetl/operations/__init__.py
@@ -1,11 +1,11 @@
 import importlib.metadata
 
+
 def get_operation(operation_type: str):
     try:
-        entrypoint = importlib.metadata.entry_points(
-            group="docetl.operation"
-        )[operation_type]
+        entrypoint = importlib.metadata.entry_points(group="docetl.operation")[
+            operation_type
+        ]
     except KeyError as e:
         raise KeyError(f"Unrecognized operation {operation_type}")
     return entrypoint.load()
- 

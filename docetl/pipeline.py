d1b14c62c457971a41c7e9e0eebba697c3ff44d9
Egil <egil.moller@freecode.no>
2024-10-05 18:27:20 +0200

    Base class for DSLRunner and Optimizer

--- docetl/pipeline.py
+++ docetl/pipeline.py
@@ -1,11 +1,12 @@
 import os
 from docetl.utils import load_config
 from typing import Any, Dict, List, Optional, Tuple, Union
-from .operations.utils import APIWrapper
+from docetl.operations.utils import APIWrapper
 from rich.console import Console
 import pyrate_limiter
 from inspect import isawaitable
 import math
+
 
 class BucketCollection(pyrate_limiter.BucketFactory):
     def __init__(self, **buckets):
@@ -28,22 +29,14 @@ class BucketCollection(pyrate_limiter.BucketFactory):
             return self.buckets["unknown"]
         return self.buckets[item.name]
 
+
 class Pipeline(object):
     @classmethod
     def from_yaml(cls, yaml_file: str, **kwargs):
-        # check that file ends with .yaml or .yml
-        if not yaml_file.endswith(".yaml") and not yaml_file.endswith(".yml"):
-            raise ValueError(
-                "Invalid file type. Please provide a YAML file ending with '.yaml' or '.yml'."
-            )
         config = load_config(yaml_file)
         return cls(config, **kwargs)
 
-    def __init__(
-        self,
-        config: Dict,
-        max_threads: int = None):
-        
+    def __init__(self, config: Dict, max_threads: int = None):
         self.config = config
         self.default_model = self.config.get("default_model", "gpt-4o-mini")
         self.console = Console()
@@ -51,15 +44,25 @@ class Pipeline(object):
         self.status = None
 
         buckets = {
-            param: pyrate_limiter.InMemoryBucket([
-                pyrate_limiter.Rate(
-                    param_limit["count"],
-                    param_limit["per"] * getattr(pyrate_limiter.Duration, param_limit.get("unit", "SECOND").upper()))
-                for param_limit in param_limits])
-            for param, param_limits in self.config.get("config", {}).get("rate_limits", {}).items()
+            param: pyrate_limiter.InMemoryBucket(
+                [
+                    pyrate_limiter.Rate(
+                        param_limit["count"],
+                        param_limit["per"]
+                        * getattr(
+                            pyrate_limiter.Duration,
+                            param_limit.get("unit", "SECOND").upper(),
+                        ),
+                    )
+                    for param_limit in param_limits
+                ]
+            )
+            for param, param_limits in self.config.get("rate_limits", {}).items()
         }
-        buckets["unknown"] = pyrate_limiter.InMemoryBucket([pyrate_limiter.Rate(math.inf, 1)])
+        buckets["unknown"] = pyrate_limiter.InMemoryBucket(
+            [pyrate_limiter.Rate(math.inf, 1)]
+        )
         bucket_factory = BucketCollection(**buckets)
         self.rate_limiter = pyrate_limiter.Limiter(bucket_factory, max_delay=math.inf)
-        
+
         self.api = APIWrapper(self)

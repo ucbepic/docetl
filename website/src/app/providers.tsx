22e2051231669e9514a809114d2b31740f6fe34d
Shreya Shankar <ss.shankar505@gmail.com>
2024-10-19 15:40:58 -0700

    change file viewer to be virtualized

---
+++ website/src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState, ReactNode } from 'react'
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}

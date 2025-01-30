e7f82216b01fbfec8fa9b5df2141bea3943c709a
Shreya Shankar <ss.shankar505@gmail.com>
2025-01-30 14:21:01 -0800

    chore: update packages

--- docetl/__init__.py
+++ docetl/__init__.py
@@ -1,5 +1,10 @@
 __version__ = "0.2.2"
 
+import warnings
+
+# TODO: Remove after https://github.com/BerriAI/litellm/issues/7560 is fixed
+warnings.filterwarnings("ignore", category=UserWarning, module="pydantic._internal._config")
+
 from docetl.runner import DSLRunner
 from docetl.optimizer import Optimizer
 from docetl.apis.pd_accessors import SemanticAccessor
